7a63f7d18755258ca45093a44c2d53a982f87d4f
Igor <igor.ss@live.com>
2022-09-21 20:54:35 -0300

    Project Costs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import Home from './components/pages/Home'
+import NewProject from './components/pages/NewProject'
+
+import Container from './components/layout/Conainer'
+import NavBar from './components/layout/NavBar'
+import Footer from './components/layout/Footer'
+import Projects from './components/pages/Projects'
+
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
-  );
+    <Router>
+      <NavBar />
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path='/'           element={<Home />}      />
+          <Route exact path='/projects'   element={<Projects />}  />
+          <Route exact path='/company'    element={<Company />}   />
+          <Route exact path='/contact'    element={<Contact />}   />
+          <Route exact path='/newproject' element={<NewProject />}/>
+        </Routes>
+      </Container>
+      <Footer />
+    </Router>
+  )
 }
 
-export default App;
+export default App
