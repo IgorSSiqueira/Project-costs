c90161b4dd2304971fd46a7231ad00a41b27c19e
Igor <igor.ss@live.com>
2022-09-23 07:08:56 -0300

    Metade do projeto

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,27 +1,49 @@
+import { useEffect, useState } from 'react'
+
 import Input from '../Form/Input'
 import Select from '../Form/Select'
+import SubmitButton from '../Form/SubmitButton'
 
 import style from './ProjectForm.module.css'
 
-function ProjectForm() {
+function ProjectForm({ btnText }) {
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:5000/categories', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setCategories(data)
+            })
+            .catch((err) => console.log(err))
+    }, [])
+
     return (
         <form className={style.form}>
-            <Input 
-                type='text' 
-                text='Nome do projeto' 
-                name='name'    
+            <Input
+                type='text'
+                text='Nome do projeto'
+                name='name'
                 placeholder='Insira o nome do projeto'
             />
-            <Input 
-                type='number' 
-                text='Orçamento do projeto' 
-                name='budget'    
+            <Input
+                type='number'
+                text='Orçamento do projeto'
+                name='budget'
                 placeholder='Insira o nome orçamento total'
             />
-            <Select name='category_ig' text='Selecione a categoria' />
-            <div>
-                <input type='submit' value='Criar projeto' />
-            </div>
+            <Select
+                name='category_ig'
+                text='Selecione a categoria'
+                options={categories}
+            />
+            <SubmitButton text={btnText} />
         </form>
     )
 }
